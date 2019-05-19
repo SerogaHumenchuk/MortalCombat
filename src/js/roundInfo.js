44a34dfdd65bade82c6e081f14f09e57e26edbe3
GrishaMokin13 <43414790+GrishaMokin13@users.noreply.github.com>
2019-05-19 09:25:10 +0300

    console

--- src/js/roundInfo.js
+++ src/js/roundInfo.js
@@ -1,1 +1,20 @@
 // функция вывода информации о раунде
+const ADForm = document.querySelector('.attack-defense');
+let attack;
+let defense;
+let output;
+const fightFunc = e => {
+  // console.log(e);
+  e.preventDefault();
+  let attack = document.querySelector('[name="defense"]:checked').value;
+  let defense = document.querySelector('[name="attack"]:checked').value;
+  console.log(attack);
+  console.log(defense);
+  if(attack === null || defense === null){
+      output = 'MAKE A CHOISE';
+  }else{
+  output = `The varname hit Bot in ${attack} and protect ${defense}`;}
+  console.log(output);
+  ADForm.reset();
+};
+ADForm.addEventListener('submit', fightFunc);
