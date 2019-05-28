0e48ed1c332afd42ddfbb2f89e22f265778252b5
GrishaMokin13 <43414790+GrishaMokin13@users.noreply.github.com>
2019-05-28 17:20:03 +0300

    fix button hit

--- src/js/roundInfo.js
+++ src/js/roundInfo.js
@@ -1,55 +1,65 @@
 // функция вывода информации о раунде
 const ADForm = document.querySelector('.attack-defense');
 const punchBut = document.querySelector('.punch-button');
-let attack, defense, output;
-let atchecks = document.querySelectorAll('[name="attack"]');
-let defchecks = document.querySelectorAll('[name="defense"]');
+let attack, defense;
 let nickname = document.querySelector('.nick_name');
 let botname = document.querySelector('.bot_name');
 
+console.log(globalObj.computer.name);
 nickname.textContent = globalObj.user.name;
 botname.textContent = globalObj.computer.name;
+
 
 function fightFunc(e) {
   e.preventDefault();
 
-  new RandomPart();
-  const fight = new FightLogic();
-  const fightAnimation = new FightAnimation();
+  let attack = document.querySelector('input[name="attack"]:checked');
+  let defense = document.querySelector('input[name="defense"]:checked');
 
-  fightAnimation.runUser();
-  fightAnimation.runComputer();
+  if (attack !== null && defense !== null) {
+    new RandomPart();
+    const fight = new FightLogic();
+    const fightAnimation = new FightAnimation();
 
-  const timerAttack = setTimeout(() => {
-    fightAnimation.attackUser();
-    fightAnimation.attackComputer();
-  }, 1280);
+    fightAnimation.runUser();
+    fightAnimation.runComputer();
 
-  const timerRunBack = setTimeout(() => {
-    if (globalObj.lifeUser <= 0) {
-      fightAnimation.dieUser();
-    } else {
-      fightAnimation.runBackUser();
-    }
+    const timerAttack = setTimeout(() => {
+      fightAnimation.attackUser();
+      fightAnimation.attackComputer();
+    }, 1280);
 
-    if (globalObj.lifeComputer <= 0) {
-      fightAnimation.dieComputer();
-    } else {
-      fightAnimation.runBackComputer();
-    }
-  }, 1840);
-  // fightAnim.runBack();
+    const timerRunBack = setTimeout(() => {
+      if (globalObj.lifeUser <= 0) {
+        fightAnimation.dieUser();
+      } else {
+        fightAnimation.runBackUser();
+      }
 
-  fight.healthUserLogic();
-  fight.healthComputerLogic();
+      if (globalObj.lifeComputer <= 0) {
+        fightAnimation.dieComputer();
+      } else {
+        fightAnimation.runBackComputer();
+      }
+    }, 1840);
+    // fightAnim.runBack();
 
-  setTimeout(() => {
-    heroLifeBar.changeHP(globalObj.lifeUser);
-    enemyLifeBar.changeHP(globalObj.lifeComputer);
-  }, 1300);
+    fight.healthUserLogic();
+    fight.healthComputerLogic();
 
-  new FightLogic();
-  ADForm.reset();
+    setTimeout(() => {
+      heroLifeBar.changeHP(globalObj.lifeUser);
+      enemyLifeBar.changeHP(globalObj.lifeComputer);
+    }, 1300);
+
+    new FightLogic();
+
+    resetTimer();
+
+    ADForm.reset();
+  } else {
+    alert('Make a choise')
+  }
 }
 ADForm.addEventListener('submit', fightFunc);
 // punchBut.addEventListener('click', resetTimerBut);
